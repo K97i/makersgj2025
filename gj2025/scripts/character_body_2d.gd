5c1132ea8c93e8c5a3d3e83bad0ebc6f7f621673
redtrec <98370912+redtrec@users.noreply.github.com>
2025-06-06 19:25:08 +0800

    basic movement

--- gj2025/scripts/character_body_2d.gd
+++ gj2025/scripts/character_body_2d.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 200.0
+const DASH_SPEED = 600.0
 const JUMP_VELOCITY = -250.0
-
+var dashing = false
+var can_dash = true
 
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,19 +15,42 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle dash.
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$dash_again_timer.start()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	
+	# Flip the sprite
 	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif direction < 0:
 		$AnimatedSprite2D.flip_h = true
-		
+	
+	# Handle player animations.
+	if is_on_floor():
+		if direction == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("jump")
+	
+	
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false # Replace with function body.
+	
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true # Replace with function body.
