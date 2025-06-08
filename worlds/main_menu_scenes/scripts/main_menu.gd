c7e72793d738996f1e9c47cb856e05d37442ae0a
K97i <105120272+K97i@users.noreply.github.com>
2025-06-09 06:52:17 +0800

    add main menu from laird branch

---
+++ worlds/main_menu_scenes/scripts/main_menu.gd
@@ -0,0 +1,27 @@
+extends Node2D
+var button_type = null
+
+func _ready():
+	$Fade_transition.show()
+	$Fade_transition/AnimationPlayer.play("fade_out")
+	$"Main Theme".play()
+
+	
+func _on_start_pressed() -> void:
+	button_type = "start"
+	$Fade_transition.show()
+	$Fade_transition/Fade_timer.start()
+	$Fade_transition/AnimationPlayer.play("fade_in")
+	get_tree().change_scene_to_file("res://worlds/level_one.tscn")
+
+	pass # put intro scene here
+	
+func _on_quit_pressed() -> void:
+	$Fade_transition.show()
+	$Fade_transition/Fade_timer.start()
+	$Fade_transition/AnimationPlayer.play("fade_in")
+	get_tree().quit()
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start":
+		pass  # put intro scene here
